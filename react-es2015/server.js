191457e6f7c4a15a408d4a7af45f5152174815bc
Erik Lindblom <erik@baens.net>
2016-01-23 21:41:13 -0700

    Working example of component and server side rendering

--- react-es2015/server.js
+++ react-es2015/server.js
@@ -1,11 +1,22 @@
 import express from 'express';
 import React from 'react';
 import ReactDOMServer from 'react-dom/server';
+import {Component} from './src/component';
+import handlebars from 'express-handlebars';
+import path from 'path';
 
 const app = express();
 
+app.engine('handlebars',handlebars());
+app.set('view engine','handlebars');
+
 app.get('/',(request,response)=>{
-	response.send(ReactDOMServer.renderToString(<div>Test</div>));
+	response.render(path.join(__dirname,'index.handlebars'),{reactContent:ReactDOMServer.renderToString(<Component/>)});
 });
 
+app.get('/bundle.js',(request,response)=>{
+	response.sendFile(path.join(__dirname,'build/bundle.js'));
+});
+
+console.log('starting');
 app.listen(3000);
